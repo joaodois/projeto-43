02c4a8fae064b8501f5b896b47953f6e8a458468
joaodois <88596300+joaodois@users.noreply.github.com>
2022-05-03 16:52:20 -0300

    Add files via upload

---
+++ projeto 43/js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Performa uma tarefa em particular!", 700, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Armazena todas as informações!", 100, 270);
+  // Adicione código para exibir as outras duas perguntas.
+}
